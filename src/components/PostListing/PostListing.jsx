a9499db7d51f78313d28f78d0b82093ca30af16e
Nick <nick.tchemi@gmail.com>
2021-04-04 21:06:21 -0700

    Cards layout to postListings

--- src/components/PostListing/PostListing.jsx
+++ src/components/PostListing/PostListing.jsx
@@ -16,17 +16,33 @@ function PostListing({ postEdges }) {
   });
 
   return (
-    <div>
-      {
-        /* Your post list here. */
-        postList.map((post) => (
-          <Link to={post.path} key={post.title}>
-            <h1>{post.title}</h1>
-          </Link>
-        ))
-      }
-    </div>
+    <section class="text-gray-600 body-font">
+      <div class="container px-5 py-24 mx-auto">
+        <div class="flex flex-wrap -m-4">
+          {
+            /* Your post list here. */
+            postList.map((post) => (
+              <div class="p-4 lg:w-1/3">
+                <Link to={post.path} key={post.title}>
+                  <div class="h-full bg-gray-100 bg-opac</Link>ity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+                    <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
+                      {post.title}
+                    </h1>
+                    <p class="leading-relaxed mb-3">
+                      Photo booth fam kinfolk cold-pressed sriracha leggings
+                      jianbing microdosing tousled waistcoat.
+                    </p>
+                  </div>
+                </Link>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </section>
   );
 }
 
 export default PostListing;
+
+
